ac1942429f5d36fc46ab00055623fd1f3776ab7e
max furman <mx.furman@gmail.com>
2018-08-01 17:25:45 -0700

    updated the shasum to match the release

--- step.rb
+++ step.rb
@@ -2,7 +2,7 @@ class Step < Formula
   desc ""
   homepage ""
   url "https://s3-us-west-1.amazonaws.com/smallstep-downloads/cli-0.0.1-rc.3.tar.gz"
-  sha256 "7aaca3b1b72a02c6668847ad142cfb9d7255ac7c06de0b242b5e2249b8b17db0"
+  sha256 "016772a194579ca982efa0d4682597e3b85d953cd4179b5d9782cab30d113c46"
 
   depends_on "dep" => :build
   depends_on "go" => :build
