1bd813b8ae9d44c984d6267ce3686dc3854f93d6
max furman <mx.furman@gmail.com>
2018-08-01 20:36:57 -0700

    copy formula from similar binary: vegeta

--- step.rb
+++ step.rb
@@ -1,5 +1,5 @@
 class Step < Formula
-  desc "Need to fill this in"
+  desc "Crypto and x509 Swiss-Army-Knife"
   homepage "https://github.com/smallstep/cli"
   url "https://s3-us-west-1.amazonaws.com/smallstep-downloads/cli-0.0.1-rc.3.tar.gz"
   sha256 "016772a194579ca982efa0d4682597e3b85d953cd4179b5d9782cab30d113c46"
@@ -8,8 +8,13 @@ class Step < Formula
   depends_on "go" => :build
 
   def install
-    system "./.brew/build"
-    bin.install "../.gobuild/src/github.com/smallstep/cli/bin/step" => "step"
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/smallstep/cli").install buildpath.children
+    ENV.prepend_create_path "PATH", buildpath/"bin"
+    cd  "src/github.com/smallstep/cli" do
+      system "make", "build"
+      bin.install "bin/step" => "step"
+    end
   end
 
   test do
